22f7e2aca98abe0f8453ff1d933d227cb64ab869
Maksim Shinkarenko <makc.pst@gmail.com>
2019-04-23 14:12:47 +0300

    ДЗ по 1 уроку

---
+++ temperature.js
@@ -0,0 +1,11 @@
+function tempFaren(tempCelc) {
+	tempCelc = parseInt(prompt('Введите температуру по цельсию (допускаются только числа)'));
+    if (isNaN(tempCelc)) {
+    	alert('Необходимо ввести число');
+        tempFaren(tempCelc);
+    } else {
+        alert('Температура по фаренгейту составляет ' + ((9 / 5) * tempCelc + 32) + ' градусов');
+    }
+}
+
+tempFaren();
